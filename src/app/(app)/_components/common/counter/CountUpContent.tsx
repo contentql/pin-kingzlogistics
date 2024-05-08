7fe6177f9e364478933c3e92c876968af703c1de
manoj karajada <manojkarajada.mk@gmail.com>
2024-05-09 00:25:57 +0530

    chore : package json update

--- src/app/(app)/_components/common/counter/CountUpContent.tsx
+++ src/app/(app)/_components/common/counter/CountUpContent.tsx
@@ -4,9 +4,9 @@ import { useState } from 'react'
 import CountUp from 'react-countup'
 import { InView } from 'react-intersection-observer'
 
-const CountUpContent = ({ number, text, add_style = true }) => {
+const CountUpContent = ({ number, text, add_style = true }: any) => {
   const [focus, setFocus] = useState(false)
-  const visibleChangeHandler = isVisible => {
+  const visibleChangeHandler = (isVisible : any) => {
     if (isVisible) {
       if (!focus) {
         setFocus(true)
@@ -15,12 +15,15 @@ const CountUpContent = ({ number, text, add_style = true }) => {
   }
   return (
     <>
-      <CountUp start={focus ? 0 : null} end={number} duration={5}>
-        {({ countUpRef }) => (
+    
+      <CountUp 
+      //@ts-ignore
+      start={focus ? 0 : null} end={number} duration={5}>
+        {({ countUpRef } : any) => (
           <div
             className={`bd-promotion-counter-number ${add_style ? 'align-items-center justify-content-center' : ''}`}>
             <span className='counter' ref={countUpRef} />
-            <InView as='span' onChange={inView => visibleChangeHandler(inView)}>
+            <InView as='span' onChange={(inView : any) => visibleChangeHandler(inView)}>
               {text && <span className='counter-text'>{text}</span>}
             </InView>
           </div>
